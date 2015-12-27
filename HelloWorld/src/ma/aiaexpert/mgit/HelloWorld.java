7c0166e83f960574b9cd53eefb7da4746e4fd02f
mohamed hamidi <mohamed.hamidi@gmail.com>
2015-12-27 18:31:29 +0000

    Second version of my HelloWorld class

--- HelloWorld/src/ma/aiaexpert/mgit/HelloWorld.java
+++ HelloWorld/src/ma/aiaexpert/mgit/HelloWorld.java
@@ -3,7 +3,7 @@ package ma.aiaexpert.mgit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Mohame dto master Git");
 
 	}
 
